aeb9cc80853f5bf572b7809a2b057506092fec6c
Bela Bagameri <bbagameri@wayfair.com>
2021-11-11 18:05:12 +0100

    Re-enable installation repository tests

--- src/main/java/com/bbaga/githubscheduledreminderapp/repositories/GitHubInstallationRepository.java
+++ src/main/java/com/bbaga/githubscheduledreminderapp/repositories/GitHubInstallationRepository.java
@@ -1,16 +1,18 @@
 package com.bbaga.githubscheduledreminderapp.repositories;
 
+import com.bbaga.githubscheduledreminderapp.infrastructure.GitHub.AppInstallationContainer;
 import org.kohsuke.github.GHAppInstallation;
 
 import java.util.HashMap;
 import java.util.Set;
 
 public class GitHubInstallationRepository {
-    private final HashMap<Long, GHAppInstallation> installations = new HashMap<>();
+    private final HashMap<Long, AppInstallationContainer> installations = new HashMap<>();
 
     public void put(GHAppInstallation installation) {
+        AppInstallationContainer container = AppInstallationContainer.create(installation);
         synchronized (this.installations) {
-            installations.put(installation.getId(), installation);
+            installations.put(container.getId(), container);
         }
     }
 
@@ -22,7 +24,13 @@ public class GitHubInstallationRepository {
 
     public GHAppInstallation get(Long installationId) {
         synchronized (this.installations) {
-            return installations.get(installationId);
+            AppInstallationContainer container = installations.get(installationId);
+
+            if (container == null) {
+                return null;
+            }
+
+            return container.unwrap();
         }
     }
 
