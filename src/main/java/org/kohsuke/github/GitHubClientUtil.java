1e55811fe865c6ea5878d8ffc342798531ab9c1b
bbaga <bagameri.bela@gmail.com>
2022-01-22 02:52:23 +0100

    Add rate limit stats to metrics (#82)

--- src/main/java/org/kohsuke/github/GitHubClientUtil.java
+++ src/main/java/org/kohsuke/github/GitHubClientUtil.java
@@ -2,10 +2,16 @@ package org.kohsuke.github;
 
 import org.kohsuke.github.internal.Previews;
 
+import java.io.IOException;
+
 public class GitHubClientUtil {
     public static PagedSearchIterable<GHRepository> listRepositories(GitHub gitHub) {
         GitHubRequest request = ((Requester)((Requester)gitHub.createRequest().withPreview(Previews.MACHINE_MAN)).withUrlPath("/installation/repositories", new String[0])).build();
         return new PagedSearchIterable(gitHub, request, GHAppInstallationRepositoryResult.class);
+    }
+
+    public static GHRateLimit getRateLimit(GitHub gitHub) throws IOException {
+        return gitHub.getClient().getRateLimit();
     }
 
     private static class GHAppInstallationRepositoryResult extends SearchResult<GHRepository> {
